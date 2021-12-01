d9a3b1f8a2582212d81af48f269e1f9cd298c570
shakhobiddin <muxawa@gmail.com>
2021-12-01 16:47:31 +0500

    qommit

--- frontend/widgets/Shop/CategoriesWidget.php
+++ frontend/widgets/Shop/CategoriesWidget.php
@@ -23,7 +23,7 @@ class CategoriesWidget extends Widget
 
     public function run(): string
     {
-        return Html::tag('div', implode(PHP_EOL, array_map(function (Category $view) {
+        return Html::tag('div', implode(PHP_EOL, array_map(function (CategoryView $view) {
             $indent = ($view->category->depth > 1 ? str_repeat('&nbsp;&nbsp;&nbsp;', $view->category->depth - 1) . '- ' : '');
             $active = $this->active && ($this->active->id == $view->category->id || $this->active->isChildOf($view->category));
             return Html::a(
