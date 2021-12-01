7c44c9d5fff47ee8ce36e0d6e971f608b8239465
shakhobiddin <muxawa@gmail.com>
2021-12-01 15:28:34 +0500

    qommit

--- shop/entities/Shop/Product/Photo.php
+++ shop/entities/Shop/Product/Photo.php
@@ -38,7 +38,7 @@ class Photo extends ActiveRecord
         return '{{%shop_photos}}';
     }
 
-    public function behaviors(): array
+    public function behaviors()
     {
         return [
             [
@@ -53,11 +53,12 @@ class Photo extends ActiveRecord
                     'admin' => ['width' => 100, 'height' => 70],
                     'thumb' => ['width' => 640, 'height' => 480],
                     'cart_list' => ['width' => 150, 'height' => 150],
-                    'cart_widget_list' => ['width' => 57, 'height' => 57],
                     'catalog_list' => ['width' => 228, 'height' => 228],
+                    'cart_widget_list' => ['width' => 57, 'height' => 57],
                     'catalog_product_main' => ['processor' => [new WaterMarker(750, 1000, '@frontend/web/image/logo.png'), 'process']],
                     'catalog_product_additional' => ['width' => 66, 'height' => 66],
                     'catalog_origin' => ['processor' => [new WaterMarker(1024, 768, '@frontend/web/image/logo.png'), 'process']],
+
                 ],
             ],
         ];
